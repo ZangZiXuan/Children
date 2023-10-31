3c8a5748177163de75eb067ef2002d0611f32938
王山而 <346669054@qq.com>
2023-10-31 13:44:18 +0800

    建立项目基本结构

---
+++ children/src/main/java/com/blbd/children/dao/entity/Child.java
@@ -0,0 +1,18 @@
+package com.blbd.children.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author Zang Xinrui
+ * @Description TODO
+ * @Date 2023/10/31 11:27
+ * @Version 1.0
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+public class Child {
+    private String name;
+    private Integer age;
+    private Integer score;
+}
