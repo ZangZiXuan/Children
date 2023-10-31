a76e265c6ac55adcaecc2ee145e429bc1e808c2e
王山而 <346669054@qq.com>
2023-10-31 14:16:47 +0800

    添加order类对象

--- children/src/main/java/com/blbd/children/dao/entity/Child.java
+++ children/src/main/java/com/blbd/children/dao/entity/Child.java
@@ -1,18 +1,11 @@
 package com.blbd.children.dao.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
 /**
  * @Author Zang Xinrui
  * @Description TODO
- * @Date 2023/10/31 11:27
+ * @Date 2023/10/31 13:45
  * @Version 1.0
  */
-@NoArgsConstructor
-@AllArgsConstructor
 public class Child {
-    private String name;
-    private Integer age;
-    private Integer score;
+
 }
